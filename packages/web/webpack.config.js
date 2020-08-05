b37827ee5cae2aa05c366351f06fbe3c83bb14b0
Alex Birch <Birch-san@users.noreply.github.com>
2020-08-05 13:34:20 +0100

    simpler repro still

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -6,25 +6,9 @@ module.exports = {
     filename: '[name].js',
     publicPath: '/'
   },
-  resolve: {
-    symlinks: false
-  },
   devServer: {
     contentBase: './dist'
   },
   mode: 'development',
-  watch: true,
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        localLibs: {
-          test: module => module === 'lib',
-          idHint: 'localLibs',
-          chunks: 'initial',
-          enforce: true,
-          priority: 1
-        }
-      }
-    }
-  }
+  watch: true
 };
